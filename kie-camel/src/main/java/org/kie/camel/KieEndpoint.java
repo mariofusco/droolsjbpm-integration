98ae637d7d586c0f016ad6633879b3135ae072cb
Mario Fusco <mario.fusco@gmail.com>
2017-02-08 16:48:12 +0100

    camel component

---
+++ kie-camel/src/main/java/org/kie/camel/KieEndpoint.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.camel;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import org.apache.camel.Consumer;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
+import org.apache.camel.impl.DefaultEndpoint;
+
+public class KieEndpoint extends DefaultEndpoint {
+
+    public KieEndpoint(String uri, KieComponent component, KieConfiguration configuration) throws URISyntaxException, MalformedURLException {
+        super(uri, component);
+        // TODO parse configuration
+    }
+
+    @Override
+    public Producer createProducer() throws Exception {
+        return new KieProducer(this);
+    }
+
+    @Override
+    public Consumer createConsumer( Processor processor ) throws Exception {
+        throw new UnsupportedOperationException("Consumer not supported for " + getClass().getSimpleName() + " endpoint");
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+}
